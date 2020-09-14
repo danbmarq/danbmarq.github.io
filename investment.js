51a81d1b670366c3dbfb12f71d49f75f03e4a5ed
danelast <domain@fine-lit.com>
2020-09-14 11:02:08 -0400

    fixes
    fixes

--- investment.js
+++ investment.js
@@ -2,6 +2,7 @@ function startOverInv() {
     document.inv_form.inv_amt.value="15000";
     document.inv_form.inv_periods.value="3";
     document.inv_form.rtrn_rate.value="8";
+    document.inv_form.extra_cont.value="50";
 
     document.getElementById("inv_info").innerHTML="";
     document.getElementById("inv_table").innerHTML="";
@@ -12,6 +13,7 @@ function validateInv() {
     var inv_amt = document.inv_form.inv_amt.value; 
     var periods = document.inv_form.inv_periods.value;
     var rtrn_rate = document.inv_form.rtrn_rate.value;
+    var extra_cont = document.inv_form.extra_cont.value;
     
     if(inv_amt <= 0 || isNaN(Number(inv_amt)) ) {
         alert("Enter a valid loan amount.");
@@ -25,20 +27,25 @@ function validateInv() {
     else if(rtrn_rate <= 0 || isNaN(Number(rtrn_rate)) ) {
         alert("Enter a valid interest rate number.");
         document.inv_form.rtrn_rate.value="";
+    } else if(extra_cont < 0 || isNaN(Number(extra_cont))) {
+        alert("Enter a valid amount of extra contributions.");
+        document.inv_form.extra_cont.value="0";
     }
     else {
-        calculateInv(parseFloat(inv_amt), parseInt(periods), parseFloat(rtrn_rate));
+        calculateInv(parseFloat(inv_amt), parseInt(periods), parseFloat(rtrn_rate), parseFloat(extra_cont));
     }
 }
 
-function calculateInv(inv_amt, periods, rtrn_rate, extra_inv) {
-    i = rtrn_rate/100;
+function calculateInv(inv_amt, periods, rtrn_rate, extra_cont) {
+    i = (rtrn_rate/12)/100;
 
-    var ttl_rtrn = inv_amt*Math.pow((1+i), periods);
+    p = periods*12
 
-    var rtrn = ttl_rtrn-inv_amt;
+    var ttl_rtrn = inv_amt*Math.pow((1+i), p) + extra_cont * ((Math.pow((1+i), p+1) - (1+i))/ i);
 
-    var avg_rtrn = rtrn/periods;
+    var ttl_cont = extra_cont*p;
+
+    var rtrn = ttl_rtrn-inv_amt - ttl_cont;
 
     var f = function thousands_separators(num) {
         var num_parts = num.toString().split(".");
@@ -49,15 +56,15 @@ function calculateInv(inv_amt, periods, rtrn_rate, extra_inv) {
 
     var info ="";
 
-    info += "<table width='250'>";
+    info += "<table>";
     info += "<tr><td>Initial Investment:</td>";
     info += "<td align='right'> $ "+f(inv_amt) +"</td></tr>"
     info += "<tr><td>Number of Periods:</td>";
     info += "<td align='right'>"+ periods +"</td></tr>"
-    info += "<tr><td>Expected Return Rate:</td>";
+    info += "<tr><td>Return Rate:</td>";
     info += "<td align='right'>"+ rtrn_rate +"%</td></tr>"
-    info += "<tr><td>Average Return per Period:</td>";
-    info += "<td align='right'> $ "+ f(avg_rtrn.toFixed(2)) +"</td></tr>"
+    info += "<tr><td>Total Contributions:</td>";
+    info += "<td align='right'> $ "+ f(ttl_cont.toFixed(2)) +"</td></tr>"
     info += "<tr><td>Total Return:</td>";
     info += "<td align='right'> $ "+ f(rtrn.toFixed(2)) +"</td></tr>"
     info += "<tr><td>Ending Balance:</td>";
@@ -69,26 +76,28 @@ function calculateInv(inv_amt, periods, rtrn_rate, extra_inv) {
 
     var table="";
 
-    table += "<table cellpadding='15' border='2.25px'>";
+    table += "<table cellpadding='10' border='2.25px'>";
 
     var e;
     var current_balance = inv_amt;
     var counter = 1;
     var total_return = 0;
 
-    for (e = 0; e < periods; e++) {
+    for (e = 0; e < p; e++) {
 
         towards_rtrn = (i)*current_balance;
-        current_balance = current_balance + towards_rtrn;
+        current_balance = current_balance + towards_rtrn + extra_cont;
         total_return = total_return + towards_rtrn;
-        var prncp = current_balance - towards_rtrn;
+        total_cont = extra_cont*(e+1)
+        var prncp = current_balance - towards_rtrn - extra_cont;
 
         table += "<tr>";
-        table += "<td width='75' align='center'>"+ counter +"</td>";
-        table += "<td width='100' align='center'>$ "+ f(prncp.toFixed(2)) +"</td>";
-        table += "<td width='90' align='center'>$ "+ f(towards_rtrn.toFixed(2)) +"</td>";
-        table += "<td width='90' align='center'>$ "+ f(total_return.toFixed(2)) +"</td>";
-        table += "<td width='100' align='center'>$ "+ f(current_balance.toFixed(2)) +"</td>";
+        table += "<td align='center'>"+ counter +"</td>";
+        table += "<td align='center'>$ "+ f(prncp.toFixed(2)) +"</td>";
+        table += "<td align='center'>$ "+ f(towards_rtrn.toFixed(2)) +"</td>";
+        table += "<td align='center'>$ "+ f(total_cont.toFixed(2)) +"</td>";
+        table += "<td align='center'>$ "+ f(total_return.toFixed(2)) +"</td>";
+        table += "<td align='center'>$ "+ f(current_balance.toFixed(2)) +"</td>";
         table += "</tr>";
 
         counter ++;
